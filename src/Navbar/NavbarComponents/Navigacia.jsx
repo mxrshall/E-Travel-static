534d03f65587363725859594d0d455b28690735a
mxrshall <martin22.krajcovic@gmail.com>
2024-03-20 21:48:39 +0100

    Pridanie fontu

--- src/Navbar/NavbarComponents/Navigacia.jsx
+++ src/Navbar/NavbarComponents/Navigacia.jsx
@@ -1,11 +1,11 @@
 function Navigacia() {
 
     return (
-      <div className="w-11/12 bg-blue-600 flex justify-end items-center text-base text-white font-bold">
-        <p className="mx-5">Lokácie</p>
-        <p className="mx-5">Katalóg</p>
-        <p className="mx-5">Galéria</p>
-        <p className="mx-5">Kontakt</p>
+      <div className="w-11/12 bg-blue-600 flex justify-end items-center text-base text-white font-bold font-mont">
+        <p className="mx-4">Lokácie</p>
+        <p className="mx-4">Katalóg</p>
+        <p className="mx-4">Galéria</p>
+        <p className="mx-4">Kontakt</p>
       </div>
     )
   }

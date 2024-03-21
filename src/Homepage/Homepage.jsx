11b0a49d0101a5a9ea290151ea0838bd331209ca
mxrshall <martin22.krajcovic@gmail.com>
2024-03-21 16:55:30 +0100

    Pridanie homepage popisku

--- src/Homepage/Homepage.jsx
+++ src/Homepage/Homepage.jsx
@@ -1,16 +1,17 @@
-import header2 from "../images/header/header2.jpg"
+import header3 from "../images/header/header3.jpg"
 import HomepageTitle from "./HomepageComponents/HomepageTitle"
+import HomepageDescription from "./HomepageComponents/HomepageDescription"
 
 function Homepage() {
 
     return (
         <>
-            <div className="w-full h-[100vh] bg-black opacity-60 absolute">
-                <div className="w-9/12 h-[100vh] pt-[10%] pl-[5%]">
+            <div className="w-9/12 h-[100vh] pt-[10%] pl-[5%] flex flex-col absolute z-10">
                     <HomepageTitle/>
+                    <HomepageDescription/>
                 </div>
-            </div>
-            <div className="w-full h-[100vh] bg-cover bg-center" style={{ backgroundImage: `url(${header2})`}}></div>
+            <div className="w-full h-[100vh] bg-black opacity-60 absolute"></div>
+            <div className="w-full h-[100vh] bg-cover bg-center" style={{ backgroundImage: `url(${header3})`}}></div>
         </>
     )
   }

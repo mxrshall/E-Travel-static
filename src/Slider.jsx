7059693e82359c93aa70b18da175012d37d80d83
mxrshall <martin22.krajcovic@gmail.com>
2024-03-23 17:48:00 +0100

    Dokoncenie slideru

--- src/Slider.jsx
+++ src/Slider.jsx
@@ -1,31 +1,25 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
 
-import header1 from "./images/header/header1.jpg"
-import header2 from "./images/header/header2.jpg"
-import header3 from "./images/header/header3.jpg"
-import header4 from "./images/header/header4.jpg"
+import Seoul from "./images/photos/Seoul.jpg"
+import BukchonHanokVillage from "./images/photos/BukchonHanokVillage.jpg"
+import Gyeongju from "./images/photos/Gyeongju.png"
 
 const sliderimages = [
     {
-        logo: header1,
-        title: "MakyGarage",
-        link: "https://www.makygarage.sk/",
+        title: "Seoul",
+        background: Seoul,
+        link: "#",
     },
     {
-        logo: header2,
-        title: "Bánczi Roman",
-        link: "https://www.lexanga.sk/",
+        title: "Bukchon Hanok Village",
+        background: BukchonHanokVillage,
+        link: "#",
     },
     {
-        logo: header3,
-        title: "Aikido Vsetin",
-        link: "https://aikidovsetin.cz/",
-    },
-    {
-        logo: header4,
-        title: "Prepravy",
-        link: "https://nadrozmerneprepravy.sk/",
+        title: "Gyeongju",
+        background: Gyeongju,
+        link: "#",
     },
 ];
 
@@ -76,7 +70,7 @@ const Slider = () => {
                 animate={{ x: 0, opacity: 1 }}
                 initial={{ x: 100, opacity: 0 }}
                 transition={{ delay: 0.8 }}
-                onClick={prev}
+                onClick={next}
                 className="w-1/12"
             >
                 ᐊ
@@ -84,15 +78,15 @@ const Slider = () => {
             <div className="w-full h-[50vh] bg-cover">
                 <a href={sliderimages[index].link} target="_blank">
                     <motion.div
-                        className="w-full h-[50vh] bg-blue-600 rounded-xl bg-cover px-5 py-3 text-xl text-white font-bold font-mont"
+                        className="w-full h-[50vh] rounded-xl bg-cover pl-5 py-5 text-xl text-white font-bold font-mont shadow-2xl shadow-black"
                         variants={slidersettings}
                         animate="animate"
                         initial="initial"
                         exit="exit"
-                        key={sliderimages[index].logo}
+                        key={sliderimages[index].background}
                         custom={direction}
                         style={{
-                            backgroundImage: `url(${sliderimages[index].logo})`,
+                            backgroundImage: `url(${sliderimages[index].background})`,
                         }}
                     >
                         {sliderimages[index].title}
@@ -103,7 +97,7 @@ const Slider = () => {
                 animate={{ x: 0, opacity: 1 }}
                 initial={{ x: -100, opacity: 0 }}
                 transition={{ delay: 0.8 }}
-                onClick={next}
+                onClick={prev}
                 className="w-1/12"
             >
                 ᐅ

f50b11e452843a1377bad8344a9f8d1ae7ff1ff4
mxrshall <martin22.krajcovic@gmail.com>
2024-03-24 14:51:38 +0100

    Update komponentu

---
+++ src/PamiatkaComponents/PamiatkaFotka.jsx
@@ -0,0 +1,10 @@
+function PamiatkaFotka({background}) {
+
+    return (
+        <>
+            <div className="w-full h-[25vh] bg-contain bg-no-repeat bg-center my-5" style={{ backgroundImage: `url(${background})`}}></div>
+        </>
+    )
+  }
+  
+  export default PamiatkaFotka
--- src/PamiatkaComponents/PamiatkaInfo.jsx
+++ src/PamiatkaComponents/PamiatkaInfo.jsx
@@ -1,13 +1,21 @@
 import Button from "../Button"
+import PamiatkaFotka from "./PamiatkaFotka"
+
+import AbuSimbel from "../images/photos/AbuSimbel.jpg"
 
 function PamiatkaInfo() {
 
     return (
         <div className="w-full h-[100vh] flex bg-black absolute z-20">
-            <div className="w-7/12 h-[100vh] pl-16 py-16">
+            <div className="w-7/12 h-[100vh] pl-16 py-16 bg-orange-700">
+                <div className="w-full h-[80vh] bg-lime-700"></div>
                 <Button text="Prejsť na katalóg"/>
             </div>
-            <div className="w-5/12 h-[100vh] p-5"></div>
+            <div className="w-5/12 h-[100vh] p-16 flex flex-col justify-center items-center">
+                <PamiatkaFotka background={AbuSimbel}/>
+                <PamiatkaFotka background={AbuSimbel}/>
+                <PamiatkaFotka background={AbuSimbel}/>
+            </div>
         </div>
     )
   }

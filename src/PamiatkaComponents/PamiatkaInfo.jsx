2fcacd24fe805f058b780c4a72ddd93130b9e05e
mxrshall <martin22.krajcovic@gmail.com>
2024-03-24 02:48:09 +0100

    Rozsirenie komponentu

---
+++ src/PamiatkaComponents/PamiatkaInfo.jsx
@@ -0,0 +1,15 @@
+import Button from "../Button"
+
+function PamiatkaInfo() {
+
+    return (
+        <div className="w-full h-[100vh] flex bg-black absolute z-20">
+            <div className="w-7/12 h-[100vh] pl-16 py-16">
+                <Button text="Prejsť na katalóg"/>
+            </div>
+            <div className="w-5/12 h-[100vh] p-5"></div>
+        </div>
+    )
+  }
+  
+  export default PamiatkaInfo
--- src/Stranky/Pamiatka.jsx
+++ src/Stranky/Pamiatka.jsx
@@ -1,5 +1,6 @@
 import Navbar from "../Navbar/Navbar"
-import PamiatkaHeader from "../PamiatkaComponents/PamiatkaHeader"
+import PamiatkaInfo from "../PamiatkaComponents/PamiatkaInfo"
+import PamiatkaTitle from "../PamiatkaComponents/PamiatkaTitle"
 import header3 from "../images/header/header3.jpg"
 
 function Pamiatka() {
@@ -8,10 +9,13 @@ function Pamiatka() {
         <>
             <Navbar/>
             <div className="w-full h-[100vh] flex justify-center items-center absolute z-10">
-                <PamiatkaHeader title="text"/>
+                <PamiatkaTitle title="text"/>
             </div>
             <div className="w-full h-[100vh] bg-black opacity-60 absolute"></div>
             <div className="w-full h-[100vh] bg-cover bg-center" style={{ backgroundImage: `url(${header3})`}}></div>
+            <div className="w-full h-[100vh] flex justify-center items-center bg-black absolute z-20">
+                <PamiatkaInfo title="text"/>
+            </div>
         </>
     )
   }

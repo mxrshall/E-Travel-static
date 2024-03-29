66c348fe0de72ba1df933cb3b179125e9d881b9d
mxrshall <martin22.krajcovic@gmail.com>
2024-03-29 22:11:01 +0100

    Pridanie linkov

--- src/HomepageComponents/HomepageButton.jsx
+++ src/HomepageComponents/HomepageButton.jsx
@@ -1,12 +1,12 @@
 import Button from "../Button"
 
-function HomepageButton({link}) {
+function HomepageButton({link1, link2, link3}) {
 
     return (
         <div className="w-full flex pt-[3%]">
-            <Button text="O nás"/>
-            <Button text="Prejsť na katalóg"/>
-            <Button text="Zobraziť lokácie" link={link}/>
+            <Button text="O nás" link={link1}/>
+            <Button text="Prejsť na katalóg" link={link2}/>
+            <Button text="Zobraziť lokácie" link={link3}/>
         </div>
     )
   }
--- src/Stranky/Homepage.jsx
+++ src/Stranky/Homepage.jsx
@@ -12,7 +12,7 @@ function Homepage() {
             <div className="w-9/12 h-[100vh] pt-[12%] pl-[5%] flex flex-col absolute z-10">
                 <HomepageTitle/>
                 <HomepageDescription/>
-                <HomepageButton link="info"/>
+                <HomepageButton link1="onas" link2="katalog" link3="info"/>
             </div>
             <div className="w-full h-[100vh] bg-black opacity-60 absolute"></div>
             <div className="w-full h-[100vh] bg-cover bg-center" style={{ backgroundImage: `url(${header3})`}}></div>

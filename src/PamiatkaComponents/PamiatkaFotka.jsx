31a8ff7eb90c4d80de2de77b56f532c1d30fe4a1
mxrshall <martin22.krajcovic@gmail.com>
2024-04-02 01:36:43 +0200

    Pridanie jednoduchej skusobnej animacie

--- src/PamiatkaComponents/PamiatkaFotka.jsx
+++ src/PamiatkaComponents/PamiatkaFotka.jsx
@@ -1,8 +1,10 @@
+import { motion } from "framer-motion";
+
 function PamiatkaFotka({background}) {
 
     return (
         <>
-            <div className="w-full h-[25vh] bg-contain bg-no-repeat bg-center mb-5" style={{ backgroundImage: `url(${background})`}}></div>
+            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5, delay: 1 }} className="w-full h-[25vh] bg-contain bg-no-repeat bg-center mb-5" style={{ backgroundImage: `url(${background})`}}></motion.div>
         </>
     )
   }
--- src/Stranky/Pamiatka.jsx
+++ src/Stranky/Pamiatka.jsx
@@ -16,7 +16,7 @@ function Pamiatka() {
             </div>
             <div className="w-full h-[100vh] bg-black opacity-60 absolute"></div>
             <div className="w-full h-[100vh] bg-cover bg-center" style={{ backgroundImage: `url(${data.background})`}}></div>
-            <div className="w-full h-[100vh] flex justify-center items-center bg-black absolute z-20">
+            <div className="w-full h-[100vh] flex justify-center items-center bg-black relative z-20">
                 <PamiatkaInfo description={data.description} photo={data.photo}/>
             </div>
         </>

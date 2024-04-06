2ff63e679ad9eceaab1c9ee6eac48bbf4100c259
mxrshall <martin22.krajcovic@gmail.com>
2024-04-06 14:17:48 +0200

    Pridanie hover effectu na kartu pre zmenu pozicie

--- src/KatalogComponents/Karta.jsx
+++ src/KatalogComponents/Karta.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 import arrow from "../images/icons/arrow_up.svg";
 
 function Karta({title, background, description, photo}) {
@@ -14,7 +15,7 @@ function Karta({title, background, description, photo}) {
     return (
         <div className="w-[18%] h-[50vh] my-5 mx-[1%] rounded-xl" onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
             <Link to="/pamiatka" state={{ data: data }}>
-                <div className="w-full h-full flex flex-col bg-cover bg-center rounded-xl p-5" style={{ backgroundImage: `url(${background})`}}>
+                <motion.div initial={{ y: 0 }} animate={{ y: hovered ? -20 : 0 }} transition={{ duration: 0.3 }} className="w-full h-full flex flex-col bg-cover bg-center rounded-xl p-5" style={{ backgroundImage: `url(${background})`}}>
                     <div className="w-full h-1/2 hover:none">
                         <p className="text-xl text-white font-bold font-mont">{title}</p>
                     </div>
@@ -23,7 +24,7 @@ function Karta({title, background, description, photo}) {
                             <img src={arrow} className="w-[1.7rem]"></img>
                         </div>
                     )}
-                </div>
+                </motion.div>
             </Link>
         </div>
     )

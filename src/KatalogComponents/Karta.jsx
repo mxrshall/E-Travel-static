1ef09025607a99a8b00c7236b84a469245242c79
mxrshall <martin22.krajcovic@gmail.com>
2024-04-06 13:54:48 +0200

    Pridanie hoveru na karty pre katalog

--- src/KatalogComponents/Karta.jsx
+++ src/KatalogComponents/Karta.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react"
 import { Link } from "react-router-dom";
+import arrow from "../images/icons/arrow_up.svg";
 
 function Karta({title, background, description, photo}) {
+    const [hovered, setHovered] = useState(false);
     const [data, setData] = useState({
         title: title,
         background: background,
@@ -10,10 +12,15 @@ function Karta({title, background, description, photo}) {
     });
 
     return (
-        <div className="w-[18%] h-[50vh] my-5 mx-[1%] rounded-xl">
+        <div className="w-[18%] h-[50vh] my-5 mx-[1%] rounded-xl" onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
             <Link to="/pamiatka" state={{ data: data }}>
                 <div className="w-full h-full flex bg-cover bg-center rounded-xl p-5" style={{ backgroundImage: `url(${background})`}}>
                     <p className="text-xl text-white font-bold font-mont">{title}</p>
+                    {hovered && (
+                        <div className="w-full h-1/2 flex justify-end items-end hover:none">
+                            <img src={arrow} className="w-[1.5rem]"></img>
+                        </div>
+                    )}
                 </div>
             </Link>
         </div>

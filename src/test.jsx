08f08d3a4790cf02ec1218ef9c4c70cbd5f278c7
mxrshall <martin22.krajcovic@gmail.com>
2024-08-05 18:45:36 +0200

    Pridanie fotiek do gridu

--- src/test.jsx
+++ src/test.jsx
@@ -2,15 +2,19 @@ import React, { useEffect } from 'react';
 import anime from 'animejs';
 import ButtonContainer from './UniversalComponents/ButtonContainer';
 import "./index.css";
+import header1 from "../public/images/header/header1.jpg";
+import header2 from "../public/images/header/header2.jpg";
+import header3 from "../public/images/header/header3.jpg";
+import header4 from "../public/images/header/header4.jpg";
 
 const itemsData = [
     {
         content: 'Cestovná kancelária E-Travel',
         imgs: [
-            'https://picsum.photos/seed/a/700/700',
-            'https://picsum.photos/seed/b/700/700',
-            'https://picsum.photos/seed/c/700/700',
-            'https://picsum.photos/seed/d/700/700',
+            header4,
+            header2,
+            header3,
+            header1,
         ],
     },
 ];
@@ -202,7 +206,7 @@ function Test({ link }) {
     };
 
     return (
-        <div className="h-screen w-screen bg-black flex items-center justify-center text-white relative overflow-hidden">
+        <div className="h-screen w-full bg-black flex items-center justify-center text-white relative overflow-hidden">
             <div className='bg-red-600 display-flex justify-center mt-[30%] z-40'>
                 <ButtonContainer link={link}/>
             </div>
@@ -213,7 +217,7 @@ function Test({ link }) {
                     </div>
                 </div>
                 <div className="imgs relative w-3/5 pt-[60%]">
-                    <div className="grid absolute top-0 left-0 w-full h-full grid-cols-12 grid-rows-12 gap-8 transform rotate-[-20deg] opacity-65">
+                    <div className="grid absolute top-0 left-0 w-full h-full grid-cols-12 grid-rows-12 gap-4 transform rotate-[-20deg] opacity-65">
                         {itemsData[0].imgs.map((img, imgIndex) => (
                             <div key={imgIndex} className={`img absolute w-full h-full ${imgIndex === 0 ? 'col-span-4 row-span-6' : imgIndex === 1 ? 'col-start-5 col-span-8 row-span-5' : imgIndex === 2 ? 'col-span-8 row-start-7 row-span-5' : 'col-start-9 col-span-4 row-span-6'}`}>
                                 <img src={img} alt="" className="absolute top-0 w-full h-full object-cover filter brightness-110 contrast-110 saturate-130 mix-blend-screen" />

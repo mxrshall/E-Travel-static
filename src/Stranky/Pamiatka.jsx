0cf0eb3898f139b330003d596f7bf041aef3c84d
mxrshall <martin22.krajcovic@gmail.com>
2024-08-19 14:23:52 +0200

    Animacia pre pamiatka podstranku

--- src/Stranky/Pamiatka.jsx
+++ src/Stranky/Pamiatka.jsx
@@ -27,9 +27,13 @@ function Pamiatka() {
     return (
         <>
             <Navbar />
-            <div className="w-full h-[100vh] flex justify-center items-center relative z-20">
+            <motion.div
+                initial={{ opacity: 0, y: 10}}
+                animate={{ opacity: 1, y: 0}}
+                transition={{ duration: 0.5, delay: 0.2 }}
+                className="w-full h-[100vh] flex justify-center items-center relative z-20">
                 <Title title={state.title} />
-            </div>
+            </motion.div>
             <motion.div
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
@@ -39,9 +43,13 @@ function Pamiatka() {
             >
                 <div className="absolute inset-0 bg-black opacity-60 z-10"></div>
             </motion.div>
-            <div className="w-full h-[100vh] flex justify-center items-center bg-black relative z-30">
+            <motion.div 
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.5 }}
+                className="w-full h-[100vh] flex justify-center items-center bg-black relative z-30">
                 <PamiatkaInfo description={state.description} photo={state.photo} sendFotka={sendFotka} />
-            </div>
+            </motion.div>
             {open && (
                 <OverlayFotka
                     fotka={selectedFotka}

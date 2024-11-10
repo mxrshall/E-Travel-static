38f1c19fd48ebaac54c97e4b0c1079dd6fb12ea7
mxrshall <martin22.krajcovic@gmail.com>
2024-11-10 19:50:25 +0100

    Dokoncenie sekcie o nas

--- src/Stranky/Homepage.jsx
+++ src/Stranky/Homepage.jsx
@@ -42,26 +42,36 @@ function Homepage() {
             </ParallaxLayer>
             <div className='w-full h-[25%]'>
             </div>
-            <div
-              className="w-full h-[25%] overflow-hidden bg-cover bg-center flex justify-center items-center relative"
-              style={{ background: `url(${backgroundVideo})` }}
-            >
+            <div className="w-full h-[25%] overflow-hidden bg-cover bg-center flex justify-center items-center relative">
+              <video
+                className="absolute inset-0 w-full h-full object-cover"
+                autoPlay
+                loop
+                muted
+                playsInline
+                style={{ zIndex: -1 }}
+              >
+                <source src={backgroundVideo} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
               <div
                 className="absolute inset-0 z-0 flex justify-center items-center"
                 style={{
-                  background: "radial-gradient(circle, rgba(0, 0, 0, 0.5) 50%, rgba(0, 0, 0, 1) 90%), linear-gradient(to top, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 0) 60%)"
+                  background: "radial-gradient(circle, rgba(0, 0, 0, 0.5) 60%, rgba(0, 0, 0, 1) 90%), linear-gradient(to top, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 0) 60%), linear-gradient(to bottom, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 0) 60%)"
                 }}
               />
-              <div className='w-2/3 flex justify-center items-center'>
+              <div className="w-2/3 flex justify-center items-center">
                 <span className="text-base text-white font-normal font-mont text-center z-10">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a 
-                galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also 
-                the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the 
-                release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like 
-                Aldus PageMaker including versions of Lorem Ipsum.
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+                  galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also
+                  the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
+                  release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
+                  Aldus PageMaker including versions of Lorem Ipsum.
                 </span>
               </div>
+            </div>
+            <div className='w-full h-[25%] bg-red-600'>
             </div>
           </Parallax>
     );

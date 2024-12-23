3addf76fa72cf7f04fb63856cbc06d0485df3cd1
Martin Krajčovič <martin22.krajcovic@gmail.com>
2024-12-23 21:03:43 +0100

    Prerobenie footeru pre mobil

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -3,14 +3,14 @@ import { FiGithub } from "react-icons/fi";
 
 function Footer() {
   return (
-    <footer className="w-full h-16 flex px-16 absolute bottom-[0%] z-30">
+    <footer className="w-full h-20 flex flex-col justify-center items-center px-16 absolute bottom-[0%] z-30 md:flex-row md:h-16">
         <div className="w-1/6"/>
-        <div className="w-4/6 h-full flex justify-center items-center text-white font-light">
+        <div className="w-full h-full flex justify-center items-center text-white font-light text-center md:w-4/6">
             <h1>
                 ©2025 Martin Krajčovič All rights reserved.
             </h1>
         </div>
-        <div className="w-1/6 h-full flex justify-end items-center gap-3">
+        <div className="w-1/6 h-full flex justify-end items-center gap-3 my-2 md:my-0">
           <a href="https://github.com/mxrshall" target="blank">
             <FiGithub color="white" size={20} />
           </a>

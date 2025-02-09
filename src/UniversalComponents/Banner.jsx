3055f1e341a1ddb7de7c75e72e99a6c4f79f7561
Martin Krajčovič <martin22.krajcovic@gmail.com>
2025-02-09 16:09:24 +0100

    Prerobenie banneru aj do mobilnej verzie

--- src/UniversalComponents/Banner.jsx
+++ src/UniversalComponents/Banner.jsx
@@ -21,16 +21,16 @@ if (!isVisible) return null;
 
 return (
         <motion.div 
-            className="w-1/4 h-[25vh] rounded-xl bg-white fixed bottom-0 right-0 z-50 m-10 p-5"
+            className="w-full h-1/5 flex flex-col justify-center items-left rounded-none bg-white fixed bottom-0 right-0 z-50 m-0 p-5 md:rounded-xl md:w-1/4 md:m-10 md:h-1/4"
             initial={{ opacity: 0 }}
             animate={isExit === false ? { opacity: 1 } : { opacity: 0 }}
             transition={isExit === false ? { duration: 0.3, delay: 1.5 } : { duration: 0.3, delay: 0.5 }}
         >
             <p className="text-sm font-medium font-mont">
-                This website uses Google Analytics to measure the time spent on the page. The results helps to determine the average time users spend on this site.
+                This website uses Google Analytics to measure the time spent on the page. The results help determine the average time users spend on this site.
             </p>
             <button 
-                className="font-mont text-sm font-medium px-2 text-black border-2 border-black hover:bg-black hover:text-white mt-5"
+                className="w-1/2 font-mont text-sm font-medium px-2 text-black border-2 border-black hover:bg-black hover:text-white mt-5"
                 onClick={handleAccept}
             >
                 I understand

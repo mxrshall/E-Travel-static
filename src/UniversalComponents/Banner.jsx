7ec252f35d74f4bee29c8ca972ecdc023ff409f9
Martin Krajčovič <martin22.krajcovic@gmail.com>
2025-03-03 18:33:09 +0100

    Nasadenie GA a Microsft Clarity

--- src/UniversalComponents/Banner.jsx
+++ src/UniversalComponents/Banner.jsx
@@ -26,7 +26,7 @@ const handleClick = () => {
 
 return (
     <motion.div 
-        className="w-full h-1/5 flex flex-col justify-center items-left rounded-none bg-white fixed bottom-0 right-0 z-50 m-0 p-5 pt-8 md:rounded-xl md:w-1/4 md:m-10 md:h-1/4"
+        className="w-full h-2/6 flex flex-col items-left rounded-none bg-white fixed bottom-0 right-0 z-50 m-0 p-5 pt-8 md:rounded-xl md:w-1/4 md:m-10 md:h-2/5"
         initial={{ opacity: 0 }}
         animate={isExit === false ? { opacity: 1 } : { opacity: 0 }}
         transition={isExit === false ? { duration: 0.3, delay: 1.5 } : { duration: 0.3, delay: 0.5 }}
@@ -35,7 +35,7 @@ return (
             <MdOutlineClose className="cursor-pointer" color="black" size="18" onClick={handleClick} />
         </div>
         <p className="text-sm font-medium font-mont">
-            This website uses Google Analytics to measure the time spent on the page. The results help determine the average time users spend on this site.
+            This website uses Google Analytics to measure the time spent on the page. The results help determine the average time users spend on this site. Additionally, this website also uses Microsoft Clarity to gain more detailed insights through heatmaps and session recordings, helping to analyze user interactions.
         </p>
         <button 
             className="w-1/2 font-mont text-sm font-medium px-2 text-black border-2 border-black hover:bg-black hover:text-white mt-5"
